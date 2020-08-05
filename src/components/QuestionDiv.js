e08f5329443abfdea61468cd90b3e4b383f78cad
Kaleigh Spurio <kaleighastell@gmail.com>
2020-08-05 18:56:28 -0400

    put the radio buttons in a form, so now you can select one from each question

--- src/components/QuestionDiv.js
+++ src/components/QuestionDiv.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 export default function QuestionDiv(props) {
   return (
@@ -7,15 +7,41 @@ export default function QuestionDiv(props) {
         <div key={result.question}>
           <p>Category: {result.category}</p>
           <p>{result.question}</p>
-          <div className="control my-3">
-            <input className='mx-1' type='radio' name='answer' value={result.all_answers[0]}/>{result.all_answers[0]}
-            <br/>
-            <input className='mx-1' type='radio' name='answer' value={result.all_answers[1]}/>{result.all_answers[1]}
-            <br/>
-            <input className='mx-1' type='radio' name='answer' value={result.all_answers[2]}/>{result.all_answers[2]}
-            <br/>
-            <input className='mx-1' type='radio' name='answer' value={result.all_answers[3]}/>{result.all_answers[3]}
-          </div>
+          <form>
+            <div className="control my-3">
+              <input
+                className="mx-1"
+                type="radio"
+                name="answer"
+                value={result.all_answers[0]}
+              />
+              {result.all_answers[0]}
+              <br />
+              <input
+                className="mx-1"
+                type="radio"
+                name="answer"
+                value={result.all_answers[1]}
+              />
+              {result.all_answers[1]}
+              <br />
+              <input
+                className="mx-1"
+                type="radio"
+                name="answer"
+                value={result.all_answers[2]}
+              />
+              {result.all_answers[2]}
+              <br />
+              <input
+                className="mx-1"
+                type="radio"
+                name="answer"
+                value={result.all_answers[3]}
+              />
+              {result.all_answers[3]}
+            </div>
+          </form>
         </div>
       ))}
     </div>
