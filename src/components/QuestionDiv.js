83e8136a297ae4063b3782e4192c721ffb403efe
Kaleigh Spurio <kaleighastell@gmail.com>
2020-08-05 18:49:32 -0400

    added radio buttons to appear with possible answers for each question

--- src/components/QuestionDiv.js
+++ src/components/QuestionDiv.js
@@ -1,17 +1,21 @@
 import React, { useState } from 'react';
 
 export default function QuestionDiv(props) {
-  
-
   return (
     <div className="container">
       {props.apiResults.map((result) => (
         <div key={result.question}>
           <p>Category: {result.category}</p>
-          <p>{decodeURIComponent(result.question)}</p>
-          <form className="control">
-            <input type="radio" />
-          </form>
+          <p>{result.question}</p>
+          <div className="control my-3">
+            <input className='mx-1' type='radio' name='answer' value={result.all_answers[0]}/>{result.all_answers[0]}
+            <br/>
+            <input className='mx-1' type='radio' name='answer' value={result.all_answers[1]}/>{result.all_answers[1]}
+            <br/>
+            <input className='mx-1' type='radio' name='answer' value={result.all_answers[2]}/>{result.all_answers[2]}
+            <br/>
+            <input className='mx-1' type='radio' name='answer' value={result.all_answers[3]}/>{result.all_answers[3]}
+          </div>
         </div>
       ))}
     </div>
