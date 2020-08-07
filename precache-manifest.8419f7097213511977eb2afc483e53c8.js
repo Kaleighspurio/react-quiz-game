deabcf7f36eb0f948ca8c0d1a4e6f185eae28b4f
Kaleigh Spurio <kaleighastell@gmail.com>
2020-08-07 13:47:57 -0400

    Updates

---
+++ precache-manifest.8419f7097213511977eb2afc483e53c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "982191351da9d26cfdda20c9baec8c98",
+    "url": "/react-quiz-game/index.html"
+  },
+  {
+    "revision": "1c881ffa3a83b81e9c7f",
+    "url": "/react-quiz-game/static/css/2.0c0672e5.chunk.css"
+  },
+  {
+    "revision": "dbec260c89e32a896e6e",
+    "url": "/react-quiz-game/static/css/main.cb855519.chunk.css"
+  },
+  {
+    "revision": "1c881ffa3a83b81e9c7f",
+    "url": "/react-quiz-game/static/js/2.7eb884e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-quiz-game/static/js/2.7eb884e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbec260c89e32a896e6e",
+    "url": "/react-quiz-game/static/js/main.71846450.chunk.js"
+  },
+  {
+    "revision": "25db6a9ded87be43df06",
+    "url": "/react-quiz-game/static/js/runtime-main.41a28176.js"
+  }
+]);
