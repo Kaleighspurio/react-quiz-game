2107ec61657524c7900623c1fef0ae8d8e19be4f
Kaleigh Spurio <kaleighastell@gmail.com>
2020-08-12 09:57:56 -0400

    fixed the question numbering and added labels to the radios

--- src/components/QuestionDiv.js
+++ src/components/QuestionDiv.js
@@ -42,53 +42,61 @@ export default function QuestionDiv(props) {
     <div className="container">
       {props.apiResults.map((result) => (
         <div
-        className="my-4 question-div"
+          className="my-4 question-div"
           key={result.question}
           name={result.question}
           onChange={props.handleRadioChange}
         >
-          <p className="my-2 left" >Category: {result.category}</p>
-          <p className="left question" >
-            {i++}. {result.question}
+          <p className="my-2 left">Category: {result.category}</p>
+          <p className="left question">
+            {1 + i++}. {result.question}
           </p>
           <p>{displayAnswers(i)}</p>
-          <form className='form'>
+          <form className="form">
             <div className="control my-3 radio-div">
-              <input
-                className="mx-1"
-                type="radio"
-                name={'question' + i}
-                value={result.all_answers[0]}
-              />
-              {result.all_answers[0]}
+              <label>
+                <input
+                  className="mx-1"
+                  type="radio"
+                  name={'question' + i}
+                  value={result.all_answers[0]}
+                />
+                {result.all_answers[0]}
+              </label>
               <br />
-              <input
-                className="mx-1"
-                type="radio"
-                name={'question' + i}
-                value={result.all_answers[1]}
-              />
-              {result.all_answers[1]}
+              <label>
+                <input
+                  className="mx-1"
+                  type="radio"
+                  name={'question' + i}
+                  value={result.all_answers[1]}
+                />
+                {result.all_answers[1]}
+              </label>
               <br />
-              <input
-                className="mx-1"
-                type="radio"
-                name={'question' + i}
-                value={result.all_answers[2]}
-              />
-              {result.all_answers[2]}
+              <label>
+                <input
+                  className="mx-1"
+                  type="radio"
+                  name={'question' + i}
+                  value={result.all_answers[2]}
+                />
+                {result.all_answers[2]}
+              </label>
               <br />
-              <input
-                className="mx-1"
-                type="radio"
-                name={'question' + i}
-                value={result.all_answers[3]}
-              />
-              {result.all_answers[3]}
+              <label>
+                <input
+                  className="mx-1"
+                  type="radio"
+                  name={'question' + i}
+                  value={result.all_answers[3]}
+                />
+                {result.all_answers[3]}
+              </label>
             </div>
           </form>
         </div>
-      ))}
+       ))}
       {displayButton()}
     </div>
   );
